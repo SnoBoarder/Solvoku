d087e0211b8b9f5a3601d469c415f4291a3bc7bd
SnoBoarder <bttran89@gmail.com>
2014-11-03 23:43:41 -0500

    Adding new assets and base structure for game.

---
+++ Assets/Scripts/SudokuNumber.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SudokuNumber : MonoBehaviour
+{
+    private static Color TEXT_ANSWER_COLOR = new Color(242, 133, 48, 255);
+    private static Color TEXT_SETUP_COLOR = Color.white;
+
+    // editor reference
+    public TextMesh _text;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    // setup sudoku numbers
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
